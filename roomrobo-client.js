9abd0c4b2b7356a868b04ed852d06c7201eae537
宮澤 修 <m1411182@edu.cc.uec.ac.jp>
2018-03-02 16:26:14 +0900

    サブロウくん用プログラムroomrobo-client.js追加．

---
+++ roomrobo-client.js
@@ -0,0 +1,36 @@
+const talk = require('./talk');
+const io_client = require('socket.io-client');
+const client_socket = io_client('http://marisa.local:3090');
+
+const speechs = [ "どうしましたか？",
+                  "おトイレ行きたいですか？",
+                  "お腹が空きましたか？",
+                  "今日は、お外は寒いですよ？",
+                  "今おトイレはいっぱいですよ，ちょっと待ってください？",
+                  "介護士さんを呼びましょうか？",
+                ]
+
+var talk_state = false; // ロボットの状態．喋るかどうか
+
+client_socket.on('connect', () => {
+  console.log('connected');
+});
+
+client_socket.on('sheet', (sheetData) => {
+  for (let key of Object.keys(sheetData.sheet)) {
+    const sheet = sheetData.sheet[key];
+    if (sheet.state == 'off') {
+      talk_state = true;
+      return;
+    }
+    // console.log(key, sheet, sheet.state);
+  }
+  talk_state = false;
+});
+
+setInterval(() => {
+  if (talk_state) {
+    var speech = speechs[Math.floor(Math.random() * speechs.length)];
+    talk.play(speech, {volume: 50}, () => { console.log(speech); });
+  }
+}, 5000);
